6cffbedd044f37b7d76a07094b8ee1b84c459773
LohitReddy359 <kamatham.lohit@gmail.com>
2022-07-07 11:31:28 -0400

    Basic chat app working

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 const app = express();
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 const pusher = new Pusher({
-  appId: 'APP_ID',
-  key: 'APP_KEY',
-  secret: 'APP_SECRET',
-  cluster: 'APP_CLUSTER',
+  appId: '1433862',
+  key: '8412a33a0d172f3a6eb5',
+  secret: '9ff788066b637ba5bcdb',
+  cluster: 'us2',
   encrypted: true
 });
+
 app.set('PORT', process.env.PORT || 5000);
+
 
 app.post('/message', (req, res) => {
   const payload = req.body;
@@ -23,5 +28,8 @@ app.post('/message', (req, res) => {
   res.send(payload)
 });
 
+
 app.listen(app.get('PORT'), () => 
   console.log('Listening at ' + app.get('PORT')))
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
+import React, { Component, useState } from 'react';
+import axios from 'axios';
+import Pusher from 'pusher-js';
+import ChatList from './ChatList';
+import ChatBox from './ChatBox';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      username: '',
+      chats: []
+    };
+  }
+
+  componentDidMount() {
+    // prompts user to enter a username as soon as they open the app (default: Anonymous)
+    const username = window.prompt('Username: ', 'Anonymous');
+    this.setState({ username });
+
+    // new Pusher instance for the new user
+    const pusher = new Pusher('8412a33a0d172f3a6eb5', {
+      cluster: 'us2',
+      encrypted: true
+    });
+
+    // subscribing the new user to the 'chat's from the channel
+    const channel = pusher.subscribe('chat');
+
+    // binding to the events emmitted by the server
+    channel.bind('message', data => {
+      this.setState({ chats: [...this.state.chats, data], test: '' });
+    });
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    if (e.keyCode === 13) {
+      const payload = {
+        username: this.state.username,
+        message: this.state.text
+      };
+      axios.post('http://localhost:5000/message', payload);
+    } else {
+      this.setState({ text: e.target.value });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React-Pusher Chat</h1>
+        </header>
+        <section>
+          <ChatList chats={this.state.chats} />
+          <ChatBox
+            text={this.state.text}
+            username={this.state.username}
+            handleTextChange={this.handleTextChange}
+          />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
